16a40e3a040bde20f2f67139b43d78d8972ceb6f
Waseem Semaan <waseem96@outlook.com>
2020-12-18 15:36:46 +0200

    added order status log

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,3 +1,5 @@
+import { AccountService } from './../../services/account.service';
+import { LogService } from 'src/app/services/log.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -11,8 +13,10 @@ import { OrderService } from '../../services/order.service';
 })
 export class OrderComponent implements OnInit, OnDestroy {
   id: string
-  subscription: Subscription
+  orderSubscription: Subscription
+  accountSubscription
   items
+  userName
   points
   orderStatus
   pointsPrice
@@ -21,18 +25,23 @@ export class OrderComponent implements OnInit, OnDestroy {
   }
   constructor(
     private router: Router,
+    private logService: LogService,
     private route: ActivatedRoute,
+    private accountService: AccountService,
     private orderService: OrderService) { 
       this.id = this.route.snapshot.paramMap.get('id');
     }
 
   ngOnInit(): void {
-    this.subscription = this.orderService.get(this.id).subscribe(p =>{
+    this.orderSubscription = this.orderService.get(this.id).subscribe(p =>{
       this.order = p.payload.val()
       this.points = p.payload.val()['usedPoints']
       this.orderStatus = p.payload.val()['status']
       this.pointsPrice = this.points > this.order['price'] ? this.order['price'] : this.points
     } )
+    this.accountSubscription = this.accountService.getAccount().subscribe(appUser => {
+      this.userName = appUser.name
+    })
     this.items = this.orderService.getItems(this.id).valueChanges()
 
     if (this.points > this.order.price ){
@@ -46,10 +55,11 @@ export class OrderComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe()
+    this.orderSubscription.unsubscribe()
   }
 
   markAsFulfilled(){
+    this.logService.addLog(this.userName, 'orders', 'fulfilled order: ', this.id, '' )
     this.orderService.changeStatus(this.id,'Fulfilled')
   }
 
